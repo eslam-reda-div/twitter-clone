cf58afef896e3179b1b63279ef30658cb9f6e75a
eslam reda <eslam.reda.div@gmail.com>
2024-01-09 11:06:31 +0200

    test

--- pages/notifications.tsx
+++ pages/notifications.tsx
@@ -4,26 +4,12 @@ import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 
-export async function getServerSideProps(context: NextPageContext) {
-  const session = await getSession(context);
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      }
-    }
-  }
-
-  return {
-    props: {
-      session
-    }
-  }
-}
-
 const Notifications = () => {
+
+  const { data: currentUser, mutate: mutateCurrentUser } = useCurrentUser();
+
+  if (!currentUser) return null;
+
   return ( 
     <>
       <Header showBackArrow label="Notifications" />
